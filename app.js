11f0902b863d98a4b65dbe91ac5f51f9358efc7a
Deepak Yadav <deepakydv0001@gmail.com>
2022-04-05 15:35:25 +0530

    📃

---
+++ app.js
@@ -0,0 +1,9 @@
+function submit(){
+    alert("You successfully placed the order😍😇 ")
+    alert("Check your mail for more info")
+    location.reload()
+}
+function cancel(){
+    alert("You canceled the order ✅");
+    location.reload()
+}
